dd62b417995f2553008e9f0dfe8515cdb95b0f4f
sravan1946 <miniclip1956@gmail.com>
2023-10-25 20:34:13 +0530

    add sessions and logout

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, session, render_template
 import json
+import secrets
 
 app = Flask(__name__)
+app.secret_key = secrets.token_bytes(32)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -15,6 +17,8 @@ def login():
         return render_template("login.html", msg="Invalid username")
     if users[username] != password:
         return render_template("login.html", msg="Invalid password")
+    session["user"] = username
+    session["logged_in"] = True
     return render_template("index.html", msg="Login successful")
 
 @app.route("/register", methods=["POST", "GET"])
@@ -32,6 +36,38 @@ def register():
         user.write(json.dumps(users))
     return render_template("login.html", msg="Registration successful")
 
+@app.route("/logout", methods=["GET"])
+def logout():
+    session["logged_in"] = False
+    session["user"] = None
+    return render_template("login.html", msg="Logout successful")
+
+@app.route("/upload", methods=["POST", "GET"])
+def upload():
+    if request.method != "POST":
+        return render_template("upload.html", msg="Please upload")
+    if not session.get("logged_in", False):
+        return render_template("login.html", msg="Please login")
+    with open("./data/uploads.json", "r") as upload:
+        uploads = json.loads(upload.read())
+    # store each recipe as a dictionary with unique id
+    recipe = {
+        "title": request.form["title"],
+        "ingredients": request.form["ingredients"],
+        "instructions": request.form["instructions"],
+        "author": session["user"],
+        "tags": request.form["tags"],
+        "image": request.form["image"],
+        "likes": 0,
+        "dislikes": 0
+    }
+    uploads.update({len(uploads): recipe})
+    with open("./data/uploads.json", "w") as upload:
+        upload.write(json.dumps(uploads))
+    return render_template("index.html", msg="Upload successful")
+    
+
+
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    return render_template("index.html", user = session.get("user", "Anonymous"))
--- utils.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def init_DB():
-    with open("bytebash/data/users.json", "w") as users:
-        users.write(json.dumps({}))
-    with open("bytebash/data/recipes.json", "w") as recipes:
-        recipes.write(json.dumps({}))
