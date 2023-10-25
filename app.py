08894c8dae1ebeb40b6a7054d54adf319d4945e5
sravan1946 <miniclip1956@gmail.com>
2023-10-25 16:42:33 +0530

    add stuff

---
+++ app.py
@@ -0,0 +1,13 @@
+import flask
+from flask import Flask, request, jsonify, render_template
+import json
+
+app = Flask(__name__)
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method != "POST":
+        return render_template("login.html", msg="Please login in")
+    username = request.form["username"]
+    password = request.form["password"]
+    return jsonify({"username": username, "password": password})
